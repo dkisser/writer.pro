fb20e30ca909cadffa63e70f2da0ada75b9ae94b
dkisser <437631891@qq.com>
2024-12-12 18:52:45 +0800

    fix(firebase): 调整fb的配置

--- src/configs/writerpro.ts
+++ src/configs/writerpro.ts
@@ -1,12 +1,20 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCbNQC0VB8JHDA9-5P1pnrVkWS7XAxm8lQ",
-    authDomain: "aigc-network.firebaseapp.com",
-    projectId: "aigc-network",
-    storageBucket: "aigc-network.firebasestorage.app",
-    messagingSenderId: "721885124722",
-    appId: "1:721885124722:web:efc2f558f594a667608aeb"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCbNQC0VB8JHDA9-5P1pnrVkWS7XAxm8lQ",
+//     authDomain: "aigc-network.firebaseapp.com",
+//     projectId: "aigc-network",
+//     storageBucket: "aigc-network.firebasestorage.app",
+//     messagingSenderId: "721885124722",
+//     appId: "1:721885124722:web:efc2f558f594a667608aeb"
+// };
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAFrxM1Vi3bMNoeqSwWyaj_9BlJOfDwX20",
+    authDomain: "aigc-network-f70a1.firebaseapp.com",
+    projectId: "aigc-network-f70a1",
+    storageBucket: "aigc-network-f70a1.firebasestorage.app",
+    messagingSenderId: "682079084500",
+    appId: "1:682079084500:web:c483cd0d9bec556a0a4041"
+  };
 
 const writerPro = {
     tenantCode: 'us',
